870a8bcd1ef47e5787d86747bbd98bcc81f90bf1
Wilson Hsu <hsu.wilson453@gmail.com>
2020-12-10 03:23:33 -0500

    added test users file

---
+++ src/shared/users.js
@@ -0,0 +1,30 @@
+export const Users = [
+  {
+    id: 0,
+    firstName: "Ben",
+    lastName: "Maloney",
+    gender: "male",
+    age: 23,
+  },
+  {
+    id: 1,
+    firstName: "Karen",
+    lastName: "Maloney",
+    gender: "female",
+    age: 27,
+  },
+  {
+    id: 2,
+    firstName: "Lyle",
+    lastName: "Schitt",
+    gender: "male",
+    age: 63,
+  },
+  {
+    id: 3,
+    firstName: "Betty",
+    lastName: "Server",
+    gender: "female",
+    age: 58,
+  },
+];

f95a6c46a7b51c9a93d6528d058770b2d580d1c3
Wilson Hsu <hsu.wilson453@gmail.com>
2020-12-10 03:17:55 -0500

    added test myMessage objects

--- src/shared/myMessages.js
+++ src/shared/myMessages.js
@@ -1,6 +1,30 @@
 export const MYMESSAGES = [
   {
     id: 0,
+    ownerId: 1,
+    senderId: 2,
     type: "encouragement",
+    text: "You are going to win that basketball game",
+  },
+  {
+    id: 1,
+    ownerId: 1,
+    senderId: 2,
+    type: "love",
+    text: "you're the most beautiful person in the world",
+  },
+  {
+    id: 2,
+    ownerId: 2,
+    senderId: 1,
+    type: "throwbackthursday",
+    text: "when we went to the movies",
+  },
+  {
+    id: 3,
+    ownerId: 2,
+    senderId: 1,
+    type: "womancrushwednesday",
+    text: "you are a cutie pie",
   },
 ];
